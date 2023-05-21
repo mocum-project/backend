4856235cb8ae4deae5abf6d257a8ff7ab00ee946
박지수 <jisupark2000@gmail.com>
2023-05-21 13:42:42 +0900

    [Fix] auto-login jwt를 request Headers로 받도록 수정

--- pages/api/auto-login.ts
+++ pages/api/auto-login.ts
@@ -1,16 +1,16 @@
 import AppResponseType from '@/types/appResponseType';
 import { NextApiRequest, NextApiResponse } from 'next';
-import jwt, { JsonWebTokenError } from 'jsonwebtoken';
+import { verify } from 'jsonwebtoken';
 import withHandler from '@/lib/withHandler';
 import JwtPayloadType from '@/types/jwtPayloadType';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<AppResponseType>) {
-  const { jwt: jwtToken }: { jwt: string } = req.body;
+  const { jwt } = req.headers;
 
   try {
     // jwt 인증
     // 유효하지 않거나 만료된 토큰이면 에러 발생
-    const decode = jwt.verify(jwtToken, process.env.JWT_PRIVATE_KEY as string);
+    const decode = verify(jwt as string, process.env.JWT_PRIVATE_KEY as string);
     const { nickname } = decode as JwtPayloadType;
 
     return res.status(200).json({
