8faf01d280ff6defb9ac457746466d4a4c818311
박지수 <jisupark2000@gmail.com>
2023-05-31 01:11:28 +0900

    [Fixed] makeJwtToken 함수 수정

--- pages/api/jwt.ts
+++ pages/api/jwt.ts
@@ -3,18 +3,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
 import withHandler from '@/lib/withHandler';
 
-function makeJwtToken(payload: string | object) {
-  const privateKey = process.env.JWT_PRIVATE_KEY;
-
-  if (typeof privateKey != 'string') {
-    throw Error;
-  }
+function makeJwtToken(payload: string | object, privateKey: string) {
   const jwtToken = jwt.sign(payload, privateKey, { expiresIn: '30d' });
   return jwtToken;
 }
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ jwt: makeJwtToken({ userId: '3', nickname: '박지수' }) });
+  return res
+    .status(200)
+    .json({ jwt: makeJwtToken({ userId: '3', nickname: '박지수' }, process.env.JWT_PRIVATE_KEY as string) });
 }
 
 export default withHandler({ methods: ['GET'], handler });
