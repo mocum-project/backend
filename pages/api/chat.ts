dc653f04ae75a6031eac3f8f980e879d069a9254
박지수 <jisupark2000@gmail.com>
2023-06-28 05:36:13 +0900

    chatbot api 추가

---
+++ pages/api/chat.ts
@@ -0,0 +1,55 @@
+import AppResponseType from '@/types/appResponseType';
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler from '@/lib/withHandler';
+import axios from 'axios';
+import NextCors from 'nextjs-cors';
+
+interface OpenAiChatResponseType {
+  choices: {
+    message: {
+      role: string;
+      content: string;
+    };
+    finish_reason: string;
+  }[];
+}
+
+type PrevChats = { role: string; content: string }[];
+
+async function fetchOpenAi(prevChats: PrevChats) {
+  const response = await axios.post<OpenAiChatResponseType>(
+    'https://api.openai.com/v1/chat/completions',
+    {
+      model: 'gpt-3.5-turbo-16k-0613',
+      messages: prevChats,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+    }
+  );
+  return response.data;
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse<AppResponseType>) {
+  const { prevChats } = JSON.parse(req.body);
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  const response = await fetchOpenAi(prevChats as PrevChats);
+
+  return res.status(200).json({
+    isSuccess: true,
+    message: '성공',
+    result: {
+      content: response.choices[0].message.content,
+    },
+  });
+}
+
+export default withHandler({ methods: ['POST'], handler });
